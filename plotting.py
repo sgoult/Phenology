524f659d02f8cd557fd5e6a895b8a12ef26dc72d
Stephen Goult <stgo@pml.ac.uk>
2019-05-20 12:23:03 +0100

    adds logic to use just one year and greatly improve parameter import from a netcdf

--- plotting.py
+++ plotting.py
@@ -1,4 +1,5 @@
 
+from __future__ import print_function, division
 import matplotlib.pyplot as plt
 import netCDF4 as nc
 import numpy
@@ -19,10 +20,13 @@ def gen_plots(phen_file, lat, lon, start, stop, pdf, xsize=35, ysize=3, start_in
     lat_var = [x for x in t.variables if "lat" in x.lower()][0]
     lats = t.variables[lat_var][:]
     lons = t.variables[lon_var][:]
-    print "lat min max:", lats.min(), lats.max()
-    print "lon min max:", lons.min(), lons.max()
+    print("lat min max:", lats.min(), lats.max())
+    print("lon min max:", lons.min(), lons.max())
     lat_idx = find_nearest(lats, lat)
     lon_idx = find_nearest(lons, lon)
+    print("after conversion indexes were:")
+    print(lat_idx, lon_idx)
+    print(start, stop, lat_idx, lon_idx)
     ser =  t.variables["filled_chl"][start:stop,:,lat_idx,lon_idx]
     x = range(start, stop)
     fig1 = plt.figure(figsize=(xsize,ysize), dpi=300)
@@ -36,7 +40,9 @@ def gen_plots(phen_file, lat, lon, start, stop, pdf, xsize=35, ysize=3, start_in
     last_end = None
     last_date_start = None
     last_date_start_two = None
+    print(list(range(start_index, stop, date_seperation_per_year)))
     for year_indx, year in enumerate(range(start_index, stop, date_seperation_per_year)):
+        print(year_indx)
         if not (year >= start and year < stop):
             continue
         last_start = plt.axvline(x=year, color='red')
@@ -48,8 +54,8 @@ def gen_plots(phen_file, lat, lon, start, stop, pdf, xsize=35, ysize=3, start_in
         if not numpy.ma.is_masked(date_start) and not (date_start+year >stop and date_stop < start):
             last_date_start = plt.axvline(x=year + date_start, color='yellow')
             plt.axvline(x=year + date_max - 1, color='yellow')
-            print "primary"
-            print date_start, date_max, date_end
+            print("primary")
+            print(date_start, date_max, date_end)
             plt.axvline(x=year + date_end, color='yellow')
         date_start =  timings.variables["date_start2"][year_indx,:,lat_idx,lon_idx]
         date_max =  timings.variables["date_max2"][year_indx,:,lat_idx,lon_idx]
@@ -58,8 +64,8 @@ def gen_plots(phen_file, lat, lon, start, stop, pdf, xsize=35, ysize=3, start_in
         if not numpy.ma.is_masked(date_start)  and not (date_start+year >stop and date_stop < start):
             last_date_start_two = plt.axvline(x=year + date_start, color='purple')
             plt.axvline(x=year + date_max -1, color='purple')
-            print "secondary"
-            print date_start, date_max, date_end
+            print("secondary")
+            print(date_start, date_max, date_end)
             plt.axvline(x=year + date_end, color='purple')
 
     if last_start:
@@ -74,6 +80,14 @@ def gen_plots(phen_file, lat, lon, start, stop, pdf, xsize=35, ysize=3, start_in
     pdf.savefig(fig1)
 
 def main(args):
+    d = nc.Dataset(args.phen_file)
+    t = nc.Dataset(args.phen_file.replace("_by_date","_intermediate_products").replace("_by_maxval","_intermediate_products"))
+    steps_per_year = d.getncattr("steps_per_year")
+    time_full = t.dimensions['TIME'].size
+    if "--extend_chl_data" in d.getncattr("generation command"):
+        time_start = steps_per_year
+    else:
+        time_start = 0
     points = [p.split(",") for p in args.points]
     if not args.output_folder:
         output_file = args.phen_file.replace("nc", "pdf")
@@ -81,13 +95,24 @@ def main(args):
         output_file = os.path.join(args.output_folder, 
                                    os.path.basename(args.phen_file.replace("nc", "pdf")))
 
+    if args.specify_years:
+        print(args.specify_years)
+        if args.specify_years[0] != args.specify_years[1]:
+            list_of_years = list(range(args.specify_years[0], args.specify_years[1] + 1))
+        else:
+            list_of_years = [args.specify_years[0]]
+
+
     with PdfPages(output_file) as pdf:
         for point in points:
-            print point
-            gen_plots(args.phen_file, float(point[0]), float(point[1]), 0, 1012, pdf)
-            for year in range(0, 1012, 46):
-                print year
-                gen_plots(args.phen_file, float(point[0]), float(point[1]), year, year+46, pdf, xsize=6, ysize=6)
+            print(point)
+            gen_plots(args.phen_file, float(point[0]), float(point[1]), time_start, time_full, pdf, start_index=time_start, date_seperation_per_year=steps_per_year)
+            if not args.skip_individual_years:
+                for year_indx, year in enumerate(range(time_start, time_full, steps_per_year)):
+                    print(year)
+                    if not year_indx in list_of_years:
+                        continue
+                    gen_plots(args.phen_file, float(point[0]), float(point[1]), year, year+steps_per_year, pdf, xsize=6, ysize=6,start_index=time_start, date_seperation_per_year=steps_per_year)
 
 
 if __name__ == "__main__":
@@ -96,5 +121,7 @@ if __name__ == "__main__":
     parser.add_argument("--output_folder", default=None, help="a location to put things, if not specified default to chlorophyll file location")
     parser.add_argument("points", nargs="+", help="A point or list of points in format lat,lon lat,lon etc. E.g. 70.93,-13.11")
     parser.add_argument("--intermediate_file", default=None, help="associated intermediate file, if not given will default to replacing by_data/by_maxval with intermediate_products")
+    parser.add_argument("--skip_individual_years", default=False, action="store_true", help="only output the whole timeseries plot")
+    parser.add_argument("--specify_years", nargs=2, type=int, help="use only one year, or if two are given (e.g. 1 5) do all years between them")
     args = parser.parse_args()
     main(args)
